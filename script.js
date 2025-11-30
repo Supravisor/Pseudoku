07b65842f7fe5fcbe42e79550893d9709f7891e9
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-12-01 07:20:17 +1300

    remove background style from all selector buttons

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const selectors = document.querySelector("#selectors");
 
   selectors.addEventListener("click", (event) => {
 
+  const select = document.querySelectorAll(".select");
+
+    select.forEach(button => button.style.background = "");
+
   });
