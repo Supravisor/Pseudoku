7aeb1e831ff5e45d476cfc535399986acd659f7a
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-12-01 07:22:07 +1300

    update background colour for selected button

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const selectors = document.querySelector("#selectors");
 
   selectors.addEventListener("click", (event) => {
 
-  const select = document.querySelectorAll(".select");
+    const select = document.querySelectorAll(".select");
 
     select.forEach(button => button.style.background = "");
+
+    if (event.target.style.background === "blue") {
+      event.target.style.background = ""
+    } else {
+        event.target.style.background = "blue";
+    }
 
   });
